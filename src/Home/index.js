406e47758aba6c3d25a2b0006c002159b8933223
LucielVitorBS <luciel.vitor.bs@gmail.com>
2019-09-19 13:34:44 -0300

    branch dev pronta

--- src/Home/index.js
+++ src/Home/index.js
@@ -1,16 +1,38 @@
 import React, {Fragment} from 'react';
 import {
   SafeAreaView,
+  ScrollView,
   View,
   Text,
+  Image,
+  TouchableOpacity,
 } from 'react-native';
 
 import styles from './styles';
 
+import logo from '../assets/logoReact/logoReact.png';
+
 const App = () => {
   return (
     <SafeAreaView style={ styles.container }>
-      <Text>Home</Text>
+        <View style={ styles.header }>
+          <Image style={ styles.logoReact } source = { logo }/>
+
+          <View style={ styles.containerHeader } >
+
+            <Text style={ styles.textHeader }>Aplicativo Demo de Libs</Text>
+          
+          </View>
+        </View>
+      <ScrollView>
+        
+        <View style={ styles.content}>
+          <TouchableOpacity style={ styles.button }>
+            <Text style={ styles.textButton }>AXIOS</Text>
+
+          </TouchableOpacity>
+        </View>
+      </ScrollView>
     </SafeAreaView>
   );
 };
--- src/Home/styles.js
+++ src/Home/styles.js
@@ -3,7 +3,44 @@ import { StyleSheet } from 'react-native';
 const styles  = StyleSheet.create({
     container:{
         flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
+        alignItems: 'stretch',
     },
-})
+    header:{
+        backgroundColor: 'black',
+        height: 100,
+    },
+    logoReact:{
+        height: '100%',
+    },
+    containerHeader:{
+        ...StyleSheet.absoluteFillObject,
+        backgroundColor: 'rgba(0,0,0,0.5)',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    textHeader: {
+        fontSize: 30,
+        color: 'white',
+        fontWeight: 'bold',
+        fontStyle: 'italic',
+    },
+    content: {
+        padding: 20,
+        alignItems: 'stretch',
+        justifyContent: 'center',
+
+    },
+    button: {
+        alignItems: 'center',
+        justifyContent: 'center',
+        backgroundColor: 'grey',
+        height: 60,
+        borderRadius: 30,
+        marginBottom: 10,
+    },
+    textButton: {
+        
+    }
+});
+
+export default styles;
